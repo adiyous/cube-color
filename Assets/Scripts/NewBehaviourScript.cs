554382abf861a2a3d6fab45d2762f9da3e8290dc
Rinfa <rinfante44@gmail.com>
2016-05-19 18:01:58 -0400

    Cambio de colores a YMCK
    Se agregan teclas y m c k y se incorpora un texto explicatorio en la UI.

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			GetComponent<Renderer> ().material.color = Color.red;
+		}
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			GetComponent<Renderer>().material.color = Color.green;
+		}
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			GetComponent<Renderer>().material.color = Color.blue;
+		}
+
+		//Added by Rafa Infante YMCK
+		if (Input.GetKeyDown(KeyCode.Y))
+		{
+			GetComponent<Renderer>().material.color = Color.yellow;
+		}
+
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			GetComponent<Renderer>().material.color = Color.magenta;
+		}
+
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			GetComponent<Renderer>().material.color = Color.cyan;
+		}
+
+		if (Input.GetKeyDown(KeyCode.K))
+		{
+			GetComponent<Renderer>().material.color = Color.black;
+		}
+	}
+}
