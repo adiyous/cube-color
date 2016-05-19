70e535edd0ad201dc1c392fbef2f07e9e1cb419e
Adil <adilnewyork@gmail.com>
2016-05-19 18:33:05 -0300

    spinin cube

---
+++ Assets/move.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+//spining cube
+public class move : MonoBehaviour {
+
+	public float speed = 10f;
+
+	void Update ()
+	{
+		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+}
